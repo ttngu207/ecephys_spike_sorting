92b920704fc806e8ac38a7a139609daccd889dca
Drew Yang <drew@vathes.com>
2021-09-28 05:05:27 +0000

    fix typo

--- ecephys_spike_sorting/modules/mean_waveforms/__main__.py
+++ ecephys_spike_sorting/modules/mean_waveforms/__main__.py
@@ -94,7 +94,7 @@ def calculate_mean_waveforms(args):
         cmd_parts.append(' -samples_per_spike='+repr(args['mean_waveform_params']['samples_per_spike']))
         cmd_parts.append(' -pre_samples='+repr(args['mean_waveform_params']['pre_samples']))
         cmd_parts.append(' -num_spikes='+repr(args['mean_waveform_params']['spikes_per_epoch']))
-        cmd_parts.extend(' -snr_radius='+repr(args['mean_waveform_params']['snr_radius']))
+        cmd_parts.append(' -snr_radius='+repr(args['mean_waveform_params']['snr_radius']))
         print(cmd_parts)
 
         # make the C_Waves call
